c9275a0aec48a25c28118286814965ac2bc9dc4c
OskarVanRest <OskarVanRest@ba653a0a-7fb7-0310-a048-973b18a224e4>
2012-06-21 10:53:38 +0000

    Workaround for GMF/352271

---
+++ org.behaviorengineering.combe.graphical.diagram/src/ComBE/diagram/part/ComBEDiagramUpdater.java
@@ -0,0 +1,55 @@
+package ComBE.diagram.part;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.gmf.runtime.notation.View;
+
+/**
+* @generated
+*/
+public class ComBEDiagramUpdater {
+
+	/**
+	 * Workaround for GMF/352271
+	 */
+	public static List<ComBENodeDescriptor> getAlternativeBranch_2004SemanticChildren(View model) {
+		return Collections.emptyList();
+	}
+
+	/**
+	 * Workaround for GMF/352271
+	 */
+	public static List<ComBENodeDescriptor> getAtomicSequence_2006SemanticChildren(View model) {
+		return Collections.emptyList();
+	}
+
+	/**
+	 * Workaround for GMF/352271
+	 */
+	public static List<ComBENodeDescriptor> getNode_3002SemanticChildren(View model) {
+		return Collections.emptyList();
+	}
+
+	/**
+	 * Workaround for GMF/352271
+	 */
+	public static List<ComBENodeDescriptor> getNode_3003SemanticChildren(View model) {
+		return Collections.emptyList();
+	}
+
+	/**
+	 * Workaround for GMF/352271
+	 */
+	public static List<ComBENodeDescriptor> getNode_3001SemanticChildren(View model) {
+		return Collections.emptyList();
+	}
+
+	/**
+	 * Workaround for GMF/352271
+	 */
+	public static List<ComBENodeDescriptor> getParallelBranch_2005SemanticChildren(View model) {
+		return Collections.emptyList();
+	}
+
+}
