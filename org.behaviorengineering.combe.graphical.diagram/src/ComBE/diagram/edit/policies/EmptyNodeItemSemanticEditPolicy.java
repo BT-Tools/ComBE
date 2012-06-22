492df8f559c8e2746325445901db435c3207aaed
OskarVanRest <OskarVanRest@ba653a0a-7fb7-0310-a048-973b18a224e4>
2012-06-22 04:49:20 +0000

    Remove nodes if their compartment is empty.

---
+++ org.behaviorengineering.combe.graphical.diagram/src/ComBE/diagram/edit/policies/EmptyNodeItemSemanticEditPolicy.java
@@ -0,0 +1,17 @@
+package ComBE.diagram.edit.policies;
+
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyElementRequest;
+
+/**
+ * @generated
+ */
+public class EmptyNodeItemSemanticEditPolicy extends ComBE.diagram.edit.policies.ComBEBaseItemSemanticEditPolicy {
+
+	/**
+	* @generated NOT
+	*/
+	protected Command getDestroyElementCommand(DestroyElementRequest req) {
+		return CustomDestroyElementCommand.getCommand(this, req);
+	}
+}
