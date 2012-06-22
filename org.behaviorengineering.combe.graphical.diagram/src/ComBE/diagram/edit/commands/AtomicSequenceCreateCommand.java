cba90e3757f0532b7970140c1ed37d61f1ac6b62
OskarVanRest <OskarVanRest@ba653a0a-7fb7-0310-a048-973b18a224e4>
2012-06-22 02:36:31 +0000

    Automatically fill an empty compartment with a new node.

---
+++ org.behaviorengineering.combe.graphical.diagram/src/ComBE/diagram/edit/commands/AtomicSequenceCreateCommand.java
@@ -0,0 +1,39 @@
+package ComBE.diagram.edit.commands;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.emf.type.core.commands.EditElementCommand;
+import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
+import org.eclipse.gmf.runtime.emf.type.core.requests.IEditCommandRequest;
+
+import ComBE.AtomicSequence;
+
+/**
+ * @generated
+ */
+public class AtomicSequenceCreateCommand extends EditElementCommand {
+	
+	/**
+	* @generated NOT
+	*/
+	protected CommandResult doExecuteWithResult(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+		Resource resource = getElementToEdit().eResource();
+		ComBE.AtomicSequence newElement = ComBE.ComBEFactory.eINSTANCE.createAtomicSequence();
+
+		// Begin non-generated code
+		ComBE.Node newNode = ComBE.ComBEFactory.eINSTANCE.createStandardNode();
+		newElement.getNodes().add(newNode);
+		// End non-generated code
+
+		resource.getContents().add(newElement);
+
+		doConfigure(newElement, monitor, info);
+
+		((CreateElementRequest) getRequest()).setNewElement(newElement);
+		return CommandResult.newOKCommandResult(newElement);
+	}
+}
