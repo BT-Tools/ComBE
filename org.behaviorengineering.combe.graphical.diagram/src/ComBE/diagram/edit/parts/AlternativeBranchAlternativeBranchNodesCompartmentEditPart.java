65ba9d90882c5210cd8acf7d18eeca3603a47db8
OskarVanRest <OskarVanRest@ba653a0a-7fb7-0310-a048-973b18a224e4>
2012-06-26 04:10:54 +0000

    Remove standard {5,5,5,5} Insets of compartments

---
+++ org.behaviorengineering.combe.graphical.diagram/src/ComBE/diagram/edit/parts/AlternativeBranchAlternativeBranchNodesCompartmentEditPart.java
@@ -0,0 +1,32 @@
+package ComBE.diagram.edit.parts;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Insets;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.ListCompartmentEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.figures.ResizableCompartmentFigure;
+
+/**
+ * @generated
+ */
+public class AlternativeBranchAlternativeBranchNodesCompartmentEditPart extends ListCompartmentEditPart {
+
+	/**
+	* @generated NOT
+	*/
+	public IFigure createFigure() {
+		ResizableCompartmentFigure result = (ResizableCompartmentFigure) super.createFigure();
+		result.setTitleVisibility(false);
+		
+		// START non-generated code: remove standard {5,5,5,5} Insets of compartment
+		Insets insets = result.getContentPane().getInsets();
+		insets.top = 0;
+		insets.bottom = 0;
+		insets.left = 0;
+		insets.right = 0;
+		// END non-generated code
+		
+		return result;
+	}
+}
