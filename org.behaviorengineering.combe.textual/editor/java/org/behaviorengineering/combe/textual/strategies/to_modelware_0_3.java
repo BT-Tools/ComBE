0d2c41e5445ca2ca81c32843097a59bd936fd199
OskarVanRest <OskarVanRest@ba653a0a-7fb7-0310-a048-973b18a224e4>
2012-09-28 12:50:15 +0000

    Switched to NBL

--- org.behaviorengineering.combe.textual/editor/java/org/behaviorengineering/combe/textual/strategies/to_modelware_0_3.java
+++ org.behaviorengineering.combe.textual/editor/java/org/behaviorengineering/combe/textual/strategies/to_modelware_0_3.java
@@ -6,9 +6,9 @@ import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.strategoxt.lang.Context;
 import org.strategoxt.lang.Strategy;
 
-public class gmf_bridge_0_3 extends Strategy {
+public class to_modelware_0_3 extends Strategy {
 
-	public static gmf_bridge_0_3 instance = new gmf_bridge_0_3();
+	public static to_modelware_0_3 instance = new to_modelware_0_3();
 
 	@Override
 	public IStrategoTerm invoke(Context context, IStrategoTerm analysedAST, IStrategoTerm textFilePath, IStrategoTerm diagramFilePath, IStrategoTerm packageName) {
