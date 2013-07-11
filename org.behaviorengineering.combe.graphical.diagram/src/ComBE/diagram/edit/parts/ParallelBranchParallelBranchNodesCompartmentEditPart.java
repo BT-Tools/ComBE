69b8a275224ee0a5144670750271b5e6e1f0b30d
oskarvanrest <oskarvanrest@gmail.com>
2013-07-11 16:57:17 -0700

    rm generated code

--- org.behaviorengineering.combe.graphical.diagram/src/ComBE/diagram/edit/parts/ParallelBranchParallelBranchNodesCompartmentEditPart.java
+++ org.behaviorengineering.combe.graphical.diagram/src/ComBE/diagram/edit/parts/ParallelBranchParallelBranchNodesCompartmentEditPart.java
@@ -1,49 +1,12 @@
 package ComBE.diagram.edit.parts;
 
-import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.geometry.Insets;
-import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.gmf.runtime.diagram.ui.editparts.ListCompartmentEditPart;
-import org.eclipse.gmf.runtime.diagram.ui.editpolicies.CreationEditPolicy;
-import org.eclipse.gmf.runtime.diagram.ui.editpolicies.DragDropEditPolicy;
-import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
-import org.eclipse.gmf.runtime.diagram.ui.figures.ResizableCompartmentFigure;
-import org.eclipse.gmf.runtime.draw2d.ui.figures.ConstrainedToolbarLayout;
-import org.eclipse.gmf.runtime.notation.View;
-import org.eclipse.gmf.tooling.runtime.edit.policies.reparent.CreationEditPolicyWithCustomReparent;
 
 /**
  * @generated
  */
 public class ParallelBranchParallelBranchNodesCompartmentEditPart extends
 		ListCompartmentEditPart {
-
-	/**
-	 * @generated
-	 */
-	public static final int VISUAL_ID = 7002;
-
-	/**
-	 * @generated
-	 */
-	public ParallelBranchParallelBranchNodesCompartmentEditPart(View view) {
-		super(view);
-	}
-
-	/**
-	 * @generated
-	 */
-	protected boolean hasModelChildrenChanged(Notification evt) {
-		return false;
-	}
-
-	/**
-	 * @generated
-	 */
-	public String getCompartmentName() {
-		return ComBE.diagram.part.Messages.ParallelBranchParallelBranchNodesCompartmentEditPart_title;
-	}
 
 	/**
 	 * @generated NOT
@@ -62,33 +25,5 @@ public class ParallelBranchParallelBranchNodesCompartmentEditPart extends
 		// END non-generated code
 
 		return result;
-	}
-
-	/**
-	 * @generated
-	 */
-	protected void createDefaultEditPolicies() {
-		super.createDefaultEditPolicies();
-		installEditPolicy(
-				EditPolicyRoles.SEMANTIC_ROLE,
-				new ComBE.diagram.edit.policies.ParallelBranchParallelBranchNodesCompartmentItemSemanticEditPolicy());
-		installEditPolicy(
-				EditPolicyRoles.CREATION_ROLE,
-				new CreationEditPolicyWithCustomReparent(
-						ComBE.diagram.part.ComBEVisualIDRegistry.TYPED_INSTANCE));
-		installEditPolicy(EditPolicyRoles.DRAG_DROP_ROLE,
-				new DragDropEditPolicy());
-		installEditPolicy(
-				EditPolicyRoles.CANONICAL_ROLE,
-				new ComBE.diagram.edit.policies.ParallelBranchParallelBranchNodesCompartmentCanonicalEditPolicy());
-	}
-
-	/**
-	 * @generated
-	 */
-	protected void setRatio(Double ratio) {
-		if (getFigure().getParent().getLayoutManager() instanceof ConstrainedToolbarLayout) {
-			super.setRatio(ratio);
-		}
 	}
 }
